506e2fbeca989df1a158981e68d76f9521134a2c
Jacques 'Lawrenz' Fernandes <jacques@castiko.com>
2019-01-07 11:57:52 +0000

    Quick setup and test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Card,
+} from 'semantic-ui-react';
 
-class App extends Component {
+class App extends React.Component {
+  
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return(
+      <div style={{margin:"0.5em"}} >
+        <Card fluid >
+          <Card.Content>
+            <Card.Header content="CalEdit" />
+          </Card.Content>
+          <Card.Content>
+            Hi!
+          </Card.Content>
+        </Card>
       </div>
     );
   }
+
 }
 
 export default App;
