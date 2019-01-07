199883520922f08b6698d4f19d6d3e03bc11f341
Jacques 'Lawrenz' Fernandes <jacques@castiko.com>
2019-01-07 11:03:30 +0000

    Updates

---
+++ precache-manifest.36bfc5267c5d834d628204d3fb4b1f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JacquesFernandes/scot-calendar.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d32b51254c1ccec94927",
+    "url": "/JacquesFernandes/scot-calendar.git/static/js/runtime~main.d32b5125.js"
+  },
+  {
+    "revision": "f7fedce0e90eb85aa9bf",
+    "url": "/JacquesFernandes/scot-calendar.git/static/js/main.f7fedce0.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/JacquesFernandes/scot-calendar.git/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f7fedce0e90eb85aa9bf",
+    "url": "/JacquesFernandes/scot-calendar.git/static/css/main.bcb88b4d.chunk.css"
+  },
+  {
+    "revision": "9576a9a647379dc0b47f94c14e75d45f",
+    "url": "/JacquesFernandes/scot-calendar.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/JacquesFernandes/scot-calendar.git/precache-manifest.36bfc5267c5d834d628204d3fb4b1f7d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/JacquesFernandes/scot-calendar.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
